9a6c77a080db4d249e2366990458ca8590aa10ce
0siek <114651472+0siek@users.noreply.github.com>
2023-03-06 22:10:41 +0100

    Add files via upload

--- skrypt.js
+++ skrypt.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
     $(window).on('scroll', function() {
         var scroll = $(window).scrollTop();
         if(scroll>=50){
-            $("sticky").addClass("stickyadd");
+            $(".sticky").addClass("stickyadd");
         }else{
-            $("sticky").removeClass("stickyadd");
+            $(".sticky").removeClass("stickyadd");
         }
     })
 })
